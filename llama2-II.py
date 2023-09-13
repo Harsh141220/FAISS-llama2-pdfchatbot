ccb419b8d3f8b16dd192771c67db89b24058a14b
Harsh141220 <62830495+Harsh141220@users.noreply.github.com>
2023-09-13 12:19:57 +0530

    Update llama2-II.py

--- llama2-II.py
+++ llama2-II.py
@@ -12,26 +12,17 @@ import pickle
 import os
 #load api key lib
 import base64
+from langchain.chains import ConversationalRetrievalChain
 
 # App title
 st.set_page_config(page_title="🦙💬 Eucloid data solutions Chatbot")
-chat_history=[]
-replicate_api="r8_SfExzEDw1tiyfpKl7ADFiAyaMu1rJfB1VE5m2"
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
 pdf = st.file_uploader("Upload your PDF", type='pdf')
-# Replicate Credentials
-with st.sidebar:
-    st.title('🦙💬 Eucloid data solutions Chatbot')
-    
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.45, step=0.05)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=4096, value=512, step=8)
+chat_history=[]
+
+api_key='1a07e0a3-d59b-4b01-b643-556e5210907e'
+env='gcp-starter'
+replicate_api="r8_SfExzEDw1tiyfpKl7ADFiAyaMu1rJfB1VE5m2"
+os.environ['REPLICATE_API_TOKEN'] = replicate
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
@@ -53,8 +44,10 @@ def generate_llama2_response(prompt_input):
                 string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
                 string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    response = chain.run(input_documents = docs, question = f"{string_dialogue} {prompt_input} Assistant: ")
-    return response
+    
+    result = qa_chain({'question': prompt_input, 'chat_history': chat_history})
+    #response = chain.run(input_documents = docs, question = f"{string_dialogue} {prompt_input} Assistant: ")
+    return result['answer']
     
 
 if pdf is not None:
@@ -72,19 +65,25 @@ if pdf is not None:
         )
 
     chunks = text_splitter.split_text(text=text)
-    store_name = pdf.name[:-4]
-    if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl","rb") as f:
-                vectorstore = pickle.load(f)
-            #st.write("Already, Embeddings loaded from the your folder (disks)")
-    else:
-            #embedding (Openai methods) 
-            embeddings = HuggingFaceEmbeddings()
-            #Store the chunks part in db (vector)
-            vectorstore = FAISS.from_texts(chunks,embedding=embeddings)
-            with open(f"{store_name}.pkl","wb") as f:
-                pickle.dump(vectorstore,f)
+    pinecone.init(api_key=api_key, environment=env)
+    embeddings = HuggingFaceEmbeddings()
+    index_name = "llama2"
+    index = pinecone.Index(index_name)
+    vectordb = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+    qa_chain = ConversationalRetrievalChain.from_llm(llm,vectordb.as_retriever(search_kwargs={'k': 3}),return_source_documents=True)
+
                 
+    with st.sidebar:
+        st.title('🦙💬 Eucloid data solutions Chatbot')
+        st.subheader('Models and parameters')
+        selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+        if selected_model == 'Llama2-7B':
+            llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+        elif selected_model == 'Llama2-13B':
+            llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+        temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.45, step=0.05)
+        top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+        max_length = st.sidebar.slider('max_length', min_value=32, max_value=4096, value=512, step=8)
             
 # Store LLM generated responses
     if "messages" not in st.session_state.keys():
@@ -94,8 +93,6 @@ if pdf is not None:
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
-
-
 
 
 # User-provided prompt
