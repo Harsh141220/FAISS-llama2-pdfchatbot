b913c71894171fd101b07103bd1234f1efd85f71
Harsh141220 <62830495+Harsh141220@users.noreply.github.com>
2023-09-13 11:11:14 +0530

    Update llama2-II.py

--- llama2-II.py
+++ llama2-II.py
@@ -33,6 +33,30 @@ with st.sidebar:
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=4096, value=512, step=8)
 
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    chat_history=[]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are an analyst. Your work is to refer the document/information provided to you and provide an answer. "
+    docs = vectorstore.similarity_search(query=prompt_input,k=3)
+    llm2 = Replicate(
+        model=llm,
+    input={"temperature": temperature, "max_length": max_length, "top_p":top_p } #here temp refers to randomness of the generated text
+    )            
+    chain = load_qa_chain(llm=llm2, chain_type= "stuff")
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+                string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+                string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    response = chain.run(input_documents = docs, question = f"{string_dialogue} {prompt_input} Assistant: ")
+    return response
+    
+
 if pdf is not None:
     st.write(pdf.name)
     pdf_reader = PdfReader(pdf)
@@ -63,55 +87,33 @@ if pdf is not None:
                 
             
 # Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-    chat_history=[]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
 
-# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
-def generate_llama2_response(prompt_input):
-    string_dialogue = "You are an analyst. Your work is to refer the document/information provided to you and provide an answer. "
-    docs = vectorstore.similarity_search(query=prompt_input,k=3)
-    llm2 = Replicate(
-        model=llm,
-    input={"temperature": temperature, "max_length": max_length, "top_p":top_p } #here temp refers to randomness of the generated text
-    )            
-    chain = load_qa_chain(llm=llm2, chain_type= "stuff")
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
-                string_dialogue += "User: " + dict_message["content"] + "\n\n"
-        else:
-                string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    response = chain.run(input_documents = docs, question = f"{string_dialogue} {prompt_input} Assistant: ")
-    return response
-    
 
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
+    if prompt := st.chat_input(disabled=not replicate_api):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
 # Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Processing..."):
-            response = generate_llama2_response(prompt)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
-            placeholder.markdown(response)
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Processing..."):
+                response = generate_llama2_response(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)
